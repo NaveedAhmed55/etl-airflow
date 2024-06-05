d35aef2a5598f665588b4cf602678762c2245ee0
NaveedAhmed55 <naveedahmednarejo12@gmail.com>
2024-06-06 02:19:34 +0500

    combined_files

--- dags/ETL_toll_data.py
+++ dags/ETL_toll_data.py
@@ -4,6 +4,8 @@ import os
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 
+import pandas as pd
+
 default_args = {
     'owner': 'naveed',
     'email': 'naveedcodes@gmail.com',
@@ -14,7 +16,7 @@ default_args = {
 }
 
 @dag(
-    dag_id='ETL_toll_data_01', 
+    dag_id='ETL_toll_data_11', 
     default_args=default_args, 
     start_date=days_ago(0),
     schedule_interval='@daily',
@@ -78,10 +80,12 @@ def hello_world_etl():
             print(f"Error while processing file {infile} : {e}")
 
     @task()
-    def consolidate_data():
-
-        pass
-
+    def consolidate_data(infile,outfile):
+        try:
+            combined_csv = pd.concat([pd.read_csv(f) for f in infile], axis=1)
+            combined_csv.to_csv(outfile, index=False)
+        except Exception as e:
+            print(f"error while processing {infile} : {e}")
     @task()
     def transform_data():
         pass
@@ -92,21 +96,24 @@ def hello_world_etl():
     # input files paths
     vehicle_data = os.path.join(DESTINATION, "vehicle-data.csv")
     tollplaza_data = os.path.join(DESTINATION, "tollplaza-data.tsv")
-    fileformats = os.path.join(DESTINATION, "fileformats.txt")
     payment_data = os.path.join(DESTINATION, "payment-data.txt")
+
 
     # output files paths
     vehicle_data_output = os.path.join(DESTINATION, "csv_data.csv")
     tollplaza_data_output = os.path.join(DESTINATION, "tsv_data.csv")
-    fileformats_output = os.path.join(DESTINATION, "fixed_width_data.csv")
     payment_data_output= os.path.join(DESTINATION, "fixed_width_data.csv")
+    infiles=[vehicle_data_output,tollplaza_data_output,payment_data_output]
+    combined_file=os.path.join(DESTINATION, "newfile.csv")
+
 
     unzip_task = unzip_data(source, DESTINATION)
     extract_csv_task = extract_data_from_csv(vehicle_data, vehicle_data_output)
     extract_tsv_task = extract_data_from_tsv(tollplaza_data, tollplaza_data_output)
     extract_data_from_fixed_width_task=extract_data_from_fixed_width(payment_data,payment_data_output)
+    consolidate_data_task=consolidate_data(infiles,combined_file)
 
     # series of tasks
-    unzip_task >> [extract_csv_task,extract_tsv_task,extract_data_from_fixed_width_task]
+    unzip_task >> [extract_csv_task,extract_tsv_task,extract_data_from_fixed_width_task] >> consolidate_data_task
 
 greet_dag = hello_world_etl()
